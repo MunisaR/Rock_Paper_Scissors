095e8b734d5264fd7a0fd82537cc3cdeb0a17081
MunisaR <munisrahmatullayeva04@gmail.com>
2022-03-28 10:16:19 +0500

    сщььше

---
+++ script.js
@@ -0,0 +1,15 @@
+////// storing for future use
+//reset everything to 0
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById(scissors_div);
+
+/////// set up the core function for the computer that will use math.random to loop through an array and return that value
